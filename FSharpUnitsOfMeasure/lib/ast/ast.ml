3934bbc7426405036009e2b3f8c479ac0fdc3f37
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2024-10-01 14:09:10 +0300

    feat: add structure items to ast, append deriving show
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>

--- FSharpUnitsOfMeasure/lib/ast/ast.ml
+++ FSharpUnitsOfMeasure/lib/ast/ast.ml
@@ -10,11 +10,12 @@ type constant =
   | Const_float of float (** Float constants: [3.14], [1e+5], [5.9E-3] *)
   | Const_measure of measure_val * measure
   (** Measure constants: [5.0<cm>], [3<kg>] *)
-[@@deriving eq, show { with_path = false }]
+[@@deriving show { with_path = false }]
 
 and measure_val =
   | Mval_int of int
   | Mval_float of float
+[@@deriving show { with_path = false }]
 
 and measure =
   | Measure_ident of string (** Measure identificator: [<m>] *)
@@ -22,12 +23,14 @@ and measure =
   | Measure_div of measure * measure (** Measure division: [<m / sec>] *)
   | Measure_pow of measure * rational_exp
   (** Measure to the rational power: [<cm^3>] *)
+[@@deriving show { with_path = false }]
 
 and rational_exp =
   | Integer of int (** Integer exponent: [2] *)
   | Rational of int * int (** Rational exponent: [3/2] *)
   | Negate of rational_exp (** Negation of exponent: [-1], [-(1/2)] *)
   | Paren of rational_exp (** Parentheses around exponent: [(5)], [(-2/3)] *)
+[@@deriving show { with_path = false }]
 
 type pattern =
   | Pattern_wild (** Wildcard patterns [_] *)
@@ -40,7 +43,7 @@ type pattern =
       Invariant: [n >= 2]
     *)
   | Pattern_or of pattern * pattern (** Or patterns: [P1 | P2] *)
-[@@deriving eq, show { with_path = false }]
+[@@deriving show { with_path = false }]
 
 type rec_flag =
   | Nonrecursive
@@ -73,4 +76,14 @@ and expression =
   | Expr_apply of expression * expression (** Application [E1 E2] *)
   | Expr_match of expression * (pattern * expression) list
   (** [match E with P1 -> E1 | ... | Pn -> En] *)
+[@@deriving show { with_path = false }]
+
+type structure_item =
+  | Str_eval of expression
+  (** Structure item which is single expression: [E] *)
+  | Str_value of rec_flag * val_binding list
+  (** [Str_value(rec_flag, [(P1, E1); ...; (Pn, En)])] represents:
+      - [let P1 = E1 and ... and Pn = En] when rec_flag is Nonrecursive
+      - [let rec P1 = E1 and ... and Pn = En] when rec_flag is Recursive
+    *)
 [@@deriving show { with_path = false }]
