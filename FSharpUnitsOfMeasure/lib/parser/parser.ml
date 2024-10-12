6566306b0795605ad9d4ddf41dc64205d83ad6d8
Andrew Strelnikov <a.strelnikov@yadro.com>
2024-10-12 20:18:26 +0300

    feat: add parse bones for patterns

--- FSharpUnitsOfMeasure/lib/parser/parser.ml
+++ FSharpUnitsOfMeasure/lib/parser/parser.ml
@@ -54,3 +54,35 @@ let token1 s = take_empty1 *> s
 let stoken s = take_empty *> string s
 let stoken1 s = take_empty1 *> string s
 let brackets p = stoken "(" *> p <* stoken ")"
+
+let pwild = Pattern_wild
+let pconst x = Pattern_const x
+let pident x = Pattern_ident x
+let ptuple t = Pattern_tuple t
+let por______________N p1 p2 = Pattern_or (p1, p2)
+
+let parse_int =
+  let parse_sign =
+    choice [ stoken "+" *> return 1; stoken "-" *> return (-1); stoken "" *> return 1 ]
+  in
+  let parse_digit = take_while1 digit in
+  lift2 (fun s v -> fint @@ (s * Int.of_string v)) parse_sign parse_digit
+;;
+
+let parse_id =
+  let length_id res = String.length res = 0 in
+  let first_char res =
+    (Char.is_digit @@ String.get res 0) || (Char.is_uppercase @@ String.get res 0)
+  in
+  take_empty *> take_while1 ident_symbol
+  >>= fun res ->
+  match length_id res, keywords res, first_char res with
+  | true, _, _ -> fail "Not identifier"
+  | _, true, _ -> fail "You can not use keywords as vars"
+  | _, _, true -> fail "The first character of the identifier is a small letter"
+  | _ -> return res
+;;
+
+let parse_pconst = parse_types >>| fun x -> pconst x
+let parse_pvar = parse_id >>| fun x -> pident x
+let parse_pwild = stoken "_" *> return pwild
